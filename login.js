923edf4d6106ebfaf74fe358a7e9b72654892a6d
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2024-11-04 01:07:00 +0500

    added

--- login.js
+++ login.js
@@ -7,44 +7,56 @@ let btnLogin = document.getElementById('signInBtn')
 let forgotPass = document.getElementById('forgetPass')
 const auth = getAuth();
 
-btnLogin.addEventListener('click',()=>{
-    signInWithEmailAndPassword(auth, emailLogin.value, passwordLogin.value)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      console.log(user)
+if(btnLogin){
+    btnLogin.addEventListener('click',()=>{
+        signInWithEmailAndPassword(auth, emailLogin.value, passwordLogin.value)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user)
+          setTimeout(()=>{
+            location.href = "dashboard.html"
+        },2000)
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          console.log(errorCode)
+          const errorMessage = error.message;
+          switch (errorCode){
+            case "auth/invalid-credential":
+                alert("Invalid Credentials")
+            
+          }
+        });
     })
-    .catch((error) => {
-      const errorCode = error.code;
-      console.log(errorCode)
-      const errorMessage = error.message;
-      switch (errorCode){
-        case "auth/invalid-credential":
-            alert("Invalid Credentials")
-      }
-    });
-})
-forgotPass.addEventListener('click',()=>{
-    sendPasswordResetEmail(auth, emailLogin.value)
-  .then(() => {
-   alert("Email Sent!Check your mailbox")
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    switch (errorCode){
-        case "auth/missing-email":
-        alert("Please enter email")
-        break;
-        case "auth/invalid-email":
-        alert("Invalid Email")
-        break;
-        default:
-        console.log("hi")
-    }
-    console.log(errorCode)
-    const errorMessage = error.message;
-    // ..
-  });
-})
+}
+if(forgotPass){
+    forgotPass.addEventListener('click',()=>{
+        let promptInput = prompt("Enter Value")
+        sendPasswordResetEmail(auth, promptInput)
+      .then(() => {
+        setTimeout(()=>{
+            alert("Email sent!check your mailbox")
+        },1000)        
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        switch (errorCode){
+            case "auth/missing-email":
+            alert("Please enter email")
+            break;
+            case "auth/invalid-email":
+            alert("Invalid Email")
+            break;
+            default:
+            console.log("hi")
+        }
+        console.log(errorCode)
+        const errorMessage = error.message;
+        // ..
+      });
+    })
+}
+
 let signUpBtn = document.getElementById('signupBtn')
 signUpBtn.addEventListener("click",()=>{
     location.href = "index.html"
