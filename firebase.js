63521407ec194830294285f8e534cac6cdaacb90
JaveriyaSaleem <javeriyasaleem678@gmail.com>
2024-11-03 21:01:31 +0500

    added

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZi9QOzSTGsNmNjsiGgEaXwNcDaUd3WE4",
+  authDomain: "fir-signup-form.firebaseapp.com",
+  projectId: "fir-signup-form",
+  storageBucket: "fir-signup-form.firebasestorage.app",
+  messagingSenderId: "6481384555",
+  appId: "1:6481384555:web:c2fa04d60c3e535e114d2e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
